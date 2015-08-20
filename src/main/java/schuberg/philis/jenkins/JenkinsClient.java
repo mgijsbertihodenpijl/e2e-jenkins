7fc1646811841b8b063dfa389a4a2b31408f7e09
Martijn Gijsberti Hodenpijl <mgijsbertihodenpijl@schubergphilis.com>
2015-08-20 15:27:05 +0200

    test for login and apiToken

---
+++ src/main/java/schuberg/philis/jenkins/JenkinsClient.java
@@ -0,0 +1,77 @@
+package schuberg.philis.jenkins;
+
+import java.net.URI;
+import java.io.IOException;
+
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.AuthCache;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.BasicAuthCache;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+public class JenkinsClient {
+    private String userName;
+    private String passWord;
+    private Document doc;
+
+    public JenkinsClient(String userName, String passWord) {
+        this.passWord = passWord;
+        this.userName = userName;
+    }
+
+    public void parse(String url) throws ClientProtocolException, IOException  {
+        URI uri = URI.create(url);
+        HttpHost host = new HttpHost(uri.getHost(), uri.getPort(), uri.getScheme());
+        CredentialsProvider credsProvider = new BasicCredentialsProvider();
+        credsProvider.setCredentials(new AuthScope(uri.getHost(), uri.getPort()), new UsernamePasswordCredentials(this.userName, this.passWord));
+        AuthCache authCache = new BasicAuthCache();
+        BasicScheme basicAuth = new BasicScheme();
+        authCache.put(host, basicAuth);
+        CloseableHttpClient httpClient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider).build();
+        HttpGet httpGet = new HttpGet(uri);
+        HttpClientContext localContext = HttpClientContext.create();
+        localContext.setAuthCache(authCache);
+
+        HttpResponse response = httpClient.execute(host, httpGet, localContext);
+        this.doc = Jsoup.parse(EntityUtils.toString(response.getEntity())) ;
+    }
+
+    private Elements getElements(String selector){
+        return this.doc.select(selector);
+    }
+
+    private Element getFirstElement(String selector) {
+        return getElements(selector).first();
+    }
+
+    public Boolean getJobStatus(String job){
+        Element td = getFirstElement("#"+ job);
+        if(td != null){
+            return td.className().trim().toLowerCase().equals("job-status-blue") ? true : false;
+        }
+        return false;
+    }
+
+    public String getApiToken(){
+        Element input =  this.getFirstElement("#apiToken");
+        if(input != null){
+            return input.attr("value");
+        }
+        return "";
+    }
+}
---
+++ src/test/java/schuberg/philis/jenkins/JenkinsClientTest.java
@@ -0,0 +1,48 @@
+package schuberg.philis.jenkins;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.fail;
+
+public class JenkinsClientTest {
+
+    String baseUrl = "http://localhost:8080";
+    JenkinsClient client;
+
+    @Before
+    public void setUp(){
+        client = new JenkinsClient("admin","admin");
+    }
+
+    @Test
+    public void testSeedJob(){
+        try {
+            client.parse(baseUrl);
+            assertTrue(client.getJobStatus("job_jenkins-job-DSL-seed"));
+        } catch(Exception e){
+            e.printStackTrace();
+            fail("Exception occured " + e.getMessage() );
+        }
+    }
+
+    @Test
+    public void testGetApiToken(){
+        try {
+            client.parse(baseUrl + "/user/admin/configure");
+            String apiToken = client.getApiToken();
+            Pattern pattern = Pattern.compile("\\w+");
+            Matcher matcher = pattern.matcher(apiToken);
+            System.out.println("apiToken {" + apiToken + "}");
+            assertTrue(matcher.matches());
+        } catch(Exception e){
+            e.printStackTrace();
+            fail("Exception occured " + e.getMessage() );
+        }
+
+    }
+}
