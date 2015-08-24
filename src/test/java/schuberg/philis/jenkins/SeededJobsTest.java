16da15817fcfd7b4c27b4438696d0b78f244dc9d
Martijn Gijsberti Hodenpijl <mgijsbertihodenpijl@schubergphilis.com>
2015-08-24 11:45:40 +0200

    system property for expected seed jobs

--- src/test/java/schuberg/philis/jenkins/SeededJobsTest.java
+++ src/test/java/schuberg/philis/jenkins/SeededJobsTest.java
@@ -12,22 +12,40 @@ import static org.junit.Assert.fail;
 public class SeededJobsTest {
     String baseUrl = "http://localhost:8080";
     JenkinsClient client;
+    List<String> expected = new ArrayList<String>();
 
     @Before
     public void setUp(){
         client = new JenkinsClient("admin","admin");
+        expected = getExpectedSeededJobs();
+    }
+
+    private List<String> getExpectedSeededJobs(){
+        List<String>names = new ArrayList<String>();
+        String seeds = System.getProperty("seededJobs");
+        if(seeds != null && seeds.trim().length() > 0 ){
+            if (seeds.contains(",")){
+                String[] jobsArr = seeds.split(",");
+                for(String job : jobsArr){
+                    names.add(job.trim());
+                }
+            } else {
+                names.add(seeds.trim());
+            }
+        } else {
+            names.add("example1");
+            names.add("example2");
+            names.add("jenkins-job-DSL-seed");
+            names.add("Job-DSL-Plugin");
+            names.add("wf-1");
+        }
+        return names;
     }
 
     @Test
     public void testSeededJobs(){
         try {
             client.parse(baseUrl);
-            List<String> expected = new ArrayList<String>();
-            expected.add("example1");
-            expected.add("example2");
-            expected.add("jenkins-job-DSL-seed");
-            expected.add("Job-DSL-Plugin");
-            expected.add("wf-1");
             List<String> jobs = client.getSeededJobNames();
             assertEquals(expected, jobs);
         } catch(Exception e){
