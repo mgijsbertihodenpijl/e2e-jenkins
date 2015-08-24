84b9b690a064acd8c8dc23d6e5eb358074ca9460
Martijn Gijsberti Hodenpijl <mgijsbertihodenpijl@schubergphilis.com>
2015-08-24 13:51:08 +0200

    ignore warnings/errors during converge, add junit test results step

--- src/main/groovy/jenkins-wrapper.groovy
+++ src/main/groovy/jenkins-wrapper.groovy
@@ -2,13 +2,14 @@ node {
     stage 'checkout'
     git 'https://github.schubergphilis.com/broden/sbp_jenkins_wrapper'
     stage 'converge'
-    sh 'kitchen converge master.*71'
+    sh 'kitchen converge master.*71 || true'
     stage 'verify'
     sh 'kitchen verify master.*71'
     stage 'e2e'
     dir('e2e') {
         git 'https://github.com/mgijsbertihodenpijl/e2e-jenkins'
         sh 'mvn clean test -Dtest=JenkinsClientTest'
+        step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
     }
     stage 'destroy'
     sh 'kitchen destroy'
