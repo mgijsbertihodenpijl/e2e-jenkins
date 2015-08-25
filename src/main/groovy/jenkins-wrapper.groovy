6297d25313175a45cbd508e703cdd47a0064c3b4
Martijn Gijsberti Hodenpijl <mgijsbertihodenpijl@schubergphilis.com>
2015-08-25 09:28:14 +0200

    Configurable polling to jenkins jobs

--- src/main/groovy/jenkins-wrapper.groovy
+++ src/main/groovy/jenkins-wrapper.groovy
@@ -8,8 +8,10 @@ node {
     stage 'e2e'
     dir('e2e') {
         def jobs = 'example1,example2,jenkins-job-DSL-seed,Job-DSL-Plugin,wf-1'
+        def retry = '3'
+        def wait = '2000'
         git 'https://github.com/mgijsbertihodenpijl/e2e-jenkins'
-        sh 'mvn clean test -Dtest=JenkinsClientTest -DseededJobs=${jobs}'
+        sh 'mvn clean test -Dtest=JenkinsClientTest -DseededJobs=${jobs} -Dretry=${retry} -Dwait=${wait}'
         step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
     }
     stage 'destroy'
--- src/test/java/schuberg/philis/jenkins/JenkinsClientTest.java
+++ src/test/java/schuberg/philis/jenkins/JenkinsClientTest.java
@@ -55,36 +55,48 @@ public class JenkinsClientTest {
         try {
             String jobToken = "s22dToken23";
             client.build(baseUrl, "jenkins-job-DSL-seed", jobToken);
-            Thread.sleep(10000);
+            long wait = getWait(5000);
+            Thread.sleep(wait);
             client.build(baseUrl, "jenkins-job-DSL-seed", jobToken);
             List<String> jobs = new ArrayList<String>();
             boolean foundJobs = false;
-            for(int i = 0; i < 5 ; i++){
-                foundJobs = false;
-                System.out.println("Try to find jobs {" + i + "}");
-                Thread.sleep(5000);
+            int tries = getRetry(5);
+            for(int i = 0; i < tries; i++){
+                System.out.println("Try to find jobs, attempt " + (i + 1));
+                Thread.sleep(wait);
                 client.parse(baseUrl);
                 jobs = client.getSeededJobNames();
+                printJobs(jobs);
                 if(jobs.size() > 1){
                     if(expectedSeeds.size() == jobs.size()){
                         foundJobs = true;
                         assertEquals(expectedSeeds, jobs);
+                        break;
                     } else {
-                        System.out.println("Not all jobs loaded, try again : " + i);
+                        System.out.println("Not all jobs loaded, try again...");
+                        foundJobs = false;
                     }
                 }
             }
             if(foundJobs == false){
-                StringBuilder b = new StringBuilder();
-                for(String job : jobs){
-                    b.append(job + " ");
-                }
-                fail("Not all seeded jobs are found after 5 retries { " + b.toString() + "}");
+                System.out.print("Expected - ");
+                printJobs(expectedSeeds);
+                System.out.print("Jenkins - ");
+                printJobs(jobs);
+                fail("Not all seeded jobs are found after " + tries + " retries ");
             }
         } catch(Exception e){
             e.printStackTrace();
             fail("Exception occured " + e.getMessage() );
         }
+    }
+
+    private void printJobs(List<String>jobs){
+        StringBuilder b = new StringBuilder();
+        for(String job : jobs){
+            b.append(job + " ");
+        }
+        System.out.println("Jobs (" + jobs.size() + ") " + "[" + b.toString() + "]");
     }
 
     private List<String> getExpectedSeededJobs(){
@@ -107,5 +119,22 @@ public class JenkinsClientTest {
             names.add("wf-1");
         }
         return names;
+    }
+
+    private int getRetry(int defaultRetry){
+        String retry = System.getProperty("retry");
+        try {
+            return Integer.parseInt(retry);
+        } catch (NumberFormatException e){}
+        return defaultRetry;
+    }
+
+    private long getWait(long defaultWait){
+        String wait = System.getProperty("wait");
+        try {
+            return Long.parseLong(wait);
+        } catch (NumberFormatException e){}
+        return defaultWait;
+
     }
 }
